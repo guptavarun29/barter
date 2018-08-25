89b0e20525516ba31bce1916bb96fce0e78f1c6b
guptavarun29 <varun.gupta1998@yahoo.com>
2018-08-25 16:11:31 +0530

    images working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:brand, :description, :price, :condition, :title)
+      params.require(:item).permit(:brand, :description, :price, :condition, :title , :image)
     end
 end
